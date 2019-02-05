aa7f05c541410019a1651c47cace90d4f4deb6af
Samvit Ramadurgam <rsamvit@gmail.com>
2019-02-05 07:01:56 -0800

    add new apps to mac setup

--- mac_setup_script.sh
+++ mac_setup_script.sh
@@ -4,6 +4,7 @@
 
 # Things i need that were not on brew cask
 # safebox
+# dayone mac app (the cli tool is on cask)
 
 brew tap caskroom/cask
 
@@ -11,27 +12,36 @@ brew tap caskroom/cask
 brew cask install 1password
 brew cask install alfred
 brew cask install bartender
+brew cask install balsamiq-mockups
 brew cask install caffeine
+brew cask install contexts
 brew cask install cyberduck
+brew cask install dayone-cli
+brew cask install docker
 brew cask install daisydisk
-brew cask install screenhero
 brew cask install dash
 brew cask install discord
 brew cask install divvy
+brew cask install box
 brew cask install dropbox
+brew cask install evernote
 brew cask install flux
 brew cask install firefox
 brew cask install google-chrome
+brew cask install google-cloud-sdk
 brew cask install istat-menus
 brew cask install iterm2
 brew cask install little-snitch
 brew cask install micro-snitch
+brew cask install postman
 brew cask install java
 brew cask install keybase
 brew cask install kaleidoscope
 brew cask install livereload
-brew cask install macvim
 brew cask install magicprefs # Map 3-finger-click -> Command+left_click, 4-finger-click -> Command+w
+brew cask install macvim
+brew cask install ngrok
+brew cask install omnifocus
 brew cask install rdm
 brew cask install rescuetime
 brew cask install sabnzbd
@@ -43,15 +53,18 @@ brew cask install charles # charles proxy
 brew cask install spotify
 brew cask install sublime-text
 brew cask install the-unarchiver
-brew cask install tunnelbear
+# brew cask install tunnelbear # Do manually - has more steps
 brew cask install utorrent
 brew cask install tripmode
 brew cask install intellij-idea
 brew cask install vlc
+brew cask install visual-studio-code
 brew cask install whatsapp
+brew cask install notational-velocity
 
 # brew utilities
 brew install ack
+brew install ammonite-repl # Scala repl
 brew install axel
 brew install bison
 brew install caddy # webserver
@@ -61,16 +74,18 @@ brew install ctags
 brew install docker
 brew install erlang
 brew install flow
+brew install gimp
 brew install git
 brew install go
 brew install heroku-toolbelt
 brew install httpie
 brew install jq
+brew install kubectl
 brew install leiningen
 brew install macvim
 brew install mono # needed for c# support by youcompleteme (vim)
 brew install mysql
-brew install ngrok
+brew install mas
 brew install nmap
 brew install node
 brew install postgresql
@@ -88,6 +103,7 @@ brew install trash
 brew install vifm
 brew install wget
 brew install yarn
+brew install youtube-dl
 
 # Taps
 brew tap laurent22/massren && brew install massren
