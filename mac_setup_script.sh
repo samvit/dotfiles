8966d32710967473b175150992463424631b759d
Samvit Ramadurgam <rsamvit@gmail.com>
2022-05-01 15:19:51 -0700

    minor updates

--- mac_setup_script.sh
+++ mac_setup_script.sh
@@ -4,63 +4,63 @@
 
 # Things i need that were not on brew cask
 # safebox
-# dayone mac app (the cli tool is on cask)
+# signal -- just added without checking
+# Amphetamine (from app store)
 
 brew tap caskroom/cask
 
 # cask utilities
-brew cask install 1password
-brew cask install alfred
-brew cask install bartender
-brew cask install balsamiq-mockups
-brew cask install caffeine
-brew cask install contexts
-brew cask install cyberduck
-brew cask install dayone-cli
-brew cask install docker
-brew cask install daisydisk
-brew cask install dash
-brew cask install discord
-brew cask install divvy
-brew cask install box
-brew cask install dropbox
-brew cask install evernote
-brew cask install flux
-brew cask install firefox
-brew cask install google-chrome
-brew cask install google-cloud-sdk
-brew cask install istat-menus
-brew cask install iterm2
-brew cask install little-snitch
-brew cask install micro-snitch
-brew cask install postman
-brew cask install java
-brew cask install keybase
-brew cask install kaleidoscope
-brew cask install livereload
-brew cask install magicprefs # Map 3-finger-click -> Command+left_click, 4-finger-click -> Command+w
-brew cask install macvim
-brew cask install ngrok
-brew cask install omnifocus
-brew cask install rdm
-brew cask install rescuetime
-brew cask install sabnzbd
-brew cask install selfcontrol
-brew cask install sizeup
-brew cask install sketch
-brew cask install slack
-brew cask install charles # charles proxy
-brew cask install spotify
-brew cask install sublime-text
-brew cask install the-unarchiver
-# brew cask install tunnelbear # Do manually - has more steps
-brew cask install utorrent
-brew cask install tripmode
-brew cask install intellij-idea
-brew cask install vlc
-brew cask install visual-studio-code
-brew cask install whatsapp
-brew cask install notational-velocity
+brew install --cask 1password
+brew install --cask alfred
+brew install --cask authy
+brew install --cask bartender
+brew install --cask balsamiq-mockups
+brew install --cask brave-browser
+
+#brew install --cask contexts
+brew install --cask cyberduck
+brew install --cask docker
+brew install --cask daisydisk
+brew install --cask dash
+brew install --cask discord
+brew install --cask divvy
+brew install --cask dropbox
+brew install --cask encryptme
+brew install --cask evernote
+brew install --cask flux
+brew install --cask firefox
+brew install --cask google-chrome
+brew install --cask google-cloud-sdk
+brew install --cask istat-menus
+brew install --cask iterm2
+#brew install --cask keyboard-maestro
+brew install --cask kindle
+brew install --cask little-snitch
+brew install --cask micro-snitch
+brew install --cask postman
+brew install --cask java
+brew install --cask kaleidoscope
+brew install --cask magicprefs # Map 3-finger-click -> Command+left_click, 4-finger-click -> Command+w
+brew install --cask macvim
+brew install --cask ngrok
+brew install --cask rdm
+brew install --cask rescuetime
+brew install --cask rstudio
+brew install --cask sabnzbd
+brew install --cask selfcontrol
+brew install --cask sizeup
+brew install --cask sketch
+brew install --cask slack
+brew install --cask sonic-pi
+brew install --cask spotify
+brew install --cask sublime-text
+brew install --cask the-unarchiver
+brew install --cask utorrent
+brew install --cask tripmode
+brew install --cask intellij-idea
+brew install --cask vlc
+brew install --cask visual-studio-code
+brew install --cask whatsapp
 
 # brew utilities
 brew install ack
@@ -89,10 +89,13 @@ brew install mas
 brew install nmap
 brew install node
 brew install postgresql
-brew install python
+brew install pyenv
+brew install poetry
+brew install r
 brew install redis
 brew install ruby
 brew install rust
+brew install s3cmd
 brew install sbt
 brew install scala
 brew install skype
